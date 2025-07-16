bd4d9e77bb7c8f5fb4bb2388669f39d646c751ea
Ava june <avajune748@gmail.com>
2025-07-16 18:10:02 +0500

    Add .next/static/chunks/src_app_page_tsx_dcce3b05._.js 1752671401202

---
+++ .next/static/chunks/src_app_page_tsx_dcce3b05._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ff20dcde._.js"
+],
+    source: "dynamic"
+});
